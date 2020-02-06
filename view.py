ca52eecf278c05dd885649fe288a424091a7b5a7
Anonymouse228 <59667226+Anonymouse228@users.noreply.github.com>
2020-02-06 18:10:04 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,7 +1,64 @@
-from app import app, all
-from flask import render_template
+from app import app, all_news
+from flask import render_template, request
+from config import categories
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', all=all)
+    return render_template('index.html', all=all_news, categories=categories)
+
+
+@app.route('/search')
+def search():
+    result = []
+    data = request.args['data'].lower()
+    for i in all_news:
+        if data in i['header'].lower() or data in i['text'].lower():
+            result.append(i)
+    return render_template('index.html', all=result)
+
+
+@app.route('/daynews')
+def daynews():
+    result = []
+    for i in all_news:
+        if 'daynews' in i["section"]:
+            result.append(i)
+    return render_template('index.html', all=result, categories=categories)
+
+
+@app.route('/opinion')
+def opinion():
+    result = []
+    for i in all_news:
+        if i["section"] == 'opinion':
+            result.append(i)
+    return render_template('index.html', all=result, categories=categories)
+
+
+@app.route('/auto')
+def auto():
+    result = []
+    for i in all_news:
+        if i["section"] == 'auto':
+            result.append(i)
+    return render_template('index.html', all=result, categories=categories)
+
+
+@app.route('/tech')
+def tech():
+    result = []
+    for i in all_news:
+        if 'tech' in i["section"]:
+            result.append(i)
+    return render_template('index.html', all=result, categories=categories)
+
+
+@app.route('/realty')
+def realty():
+    result = []
+    for i in all_news:
+        if i["section"] == 'realt':
+            result.append(i)
+    return render_template('index.html', all=result, categories=categories)
+
